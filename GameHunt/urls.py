4a4e69c989ab426b7a733a2c8fb73932a76ef698
NKSMDR <nksmdr342@gmail.com>
2025-01-26 21:57:54 +0545

    before adding allgamespage

--- GameHunt/urls.py
+++ GameHunt/urls.py
@@ -4,12 +4,14 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import login_view, signup_view
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', views.home, name='home'),  # Changed from game_list to home to match your template
     path('game/<int:game_id>/', views.game_detail, name='game_detail'),  # New URL pattern for game details
     path('cart/', views.cart_view, name='cart'),
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
 
--- mywebgamestore/settings.py
+++ mywebgamestore/settings.py
@@ -136,3 +136,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
+LOGIN_REDIRECT_URL = 'home'  # Redirect to 'home' after login
